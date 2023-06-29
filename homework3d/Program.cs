4c9588543b0d9635c98b60c2b7cc371a486beedb
vahidaa <vahidaa@code.edu.az>
2023-06-30 00:38:36 +0400

    regex created

---
+++ homework3d/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&\D])[a-zA-Z\d!@#$%^&*()_\/]{8,}$";
+
+bool isValid = false;
+
+while (!isValid)
+{
+    Console.Write("Parolu daxil edin: ");
+    string password = Console.ReadLine();
+
+    isValid = Regex.IsMatch(password, pattern);
+
+    if (isValid)
+    {
+        Console.WriteLine("Parol duzgundur.");
+    }
+    else
+    {
+        Console.WriteLine("Parol sehvdi. Yeniden daxil edin.");
+    }
+}
